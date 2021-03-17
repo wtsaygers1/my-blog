7628eaa505b1b49114ad3336d293751e061c997e
wtsaygers1 <wtsaygers1@gmail.com>
2021-03-16 20:53:29 -0400

    Parent and Child

--- src/components/App.js
+++ src/components/App.js
@@ -3,9 +3,19 @@ import Header from "./Header";
 
 
 function App() {
+  let name = "Wally";
+
+  let age = 31;
+
   return (
     <div className="App ">
-    <Header />
+      <Header
+        text="howdy"
+        name={name}
+        age={age}
+        // demonstrating that a key with no value is null and will assume true
+        nerd
+      />
       App Layer Components
     </div>
   );
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,28 @@
 import React from "react";
 
-function Header() {
-    return(
-      <h1>
-        Header!
-      </h1>
-    )
+function Header(props) {
+
+  if(props.nerd){
+    console.log(`he is a nerd`)
+  } else {
+    console.log(`he is not a nerd`)
   }
 
-  export default Header;
+  console.log(props)
+  return (
+    // a way to put html elements next to each other in React
+    // <React.Fragment>
+    // <> and </> are short hand for Reach.Fragment
+    <>
+      <h1>
+        Hello {props.name}
+      </h1>
+      <h3>
+        {props.name} is {props.age} years old
+    </h3>
+    {/* </React.Fragment> */}
+    </>
+  )
+}
+
+export default Header;
