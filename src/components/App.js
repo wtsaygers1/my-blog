ecb7230481aa146c5d2d864db81b2f0161183186
wtsaygers1 <wtsaygers1@gmail.com>
2021-03-17 11:42:12 -0400

    Class and state mgmt

--- src/components/App.js
+++ src/components/App.js
@@ -1,24 +1,41 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Header from "./Header";
 
 
-function App() {
-  let name = "Wally";
+class App extends Component {
 
-  let age = 31;
+  constructor() {
+    super();
+    this.Waltsname = "Wally";
+    this.age = 31
+    // possibly could be updated in future for site visits
+    
+    //  state must be defined in the constructor and state is immutable meaning we
+    // can define things and they will stay forever until we use a special method to
+    // rename them
+    this.state = {
+      clicked: 0
+    }
+  }
 
-  return (
-    <div className="App ">
-      <Header
-        text="howdy"
-        name={name}
-        age={age}
-        // demonstrating that a key with no value is null and will assume true
-        nerd
-      />
+
+  render() {
+    return (
+      <div className="App ">
+        <Header
+          text="howdy"
+          name={this.Waltsname}
+          age={this.age}
+          // demonstrating that a key with no value is null and will assume true
+          nerd
+        />
       App Layer Components
-    </div>
-  );
+      <div>
+        clicked: {this.state.clicked}
+      </div>
+      </div>
+    )
+  }
 }
 
 export default App;
