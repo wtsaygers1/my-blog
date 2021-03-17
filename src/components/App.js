42d5b1291fec7b6e4f9bd0f3f95d7195687567cd
wtsaygers1 <wtsaygers1@gmail.com>
2021-03-16 20:21:52 -0400

    adding components vid

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Header from "./Header";
+
+
+function App() {
+  return (
+    <div className="App ">
+    <Header />
+      App Layer Components
+    </div>
+  );
+}
+
+export default App;
