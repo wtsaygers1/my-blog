4ffb63504a9c5cb81978a1909f7519ba39f7e3d9
wtsaygers1 <wtsaygers1@gmail.com>
2021-03-18 10:46:07 -0400

    dynamic render in react

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,50 +1,26 @@
 import React from "react";
 
 function Header(props) {
-
   return (
     <ul className="nav nav-pills">
-      <li className="nav-item">
-        <a 
-        onClick={() => props.setPage(0)}
-        className={"nav-link " + (props.currentPage === 0 ? "active" : "")}
-        >
-        {props.pages[0]}
-        </a>
-      </li>
-      <li className="nav-item">
-        <a 
-        onClick={() => props.setPage(1)}
-        className={"nav-link " + (props.currentPage === 1 ? "active" : "")}
-        >
-        {props.pages[1]}
-        </a>
-      </li>
-      <li className="nav-item">
-        <a 
-        onClick={() => props.setPage(2)}
-        className={"nav-link " + (props.currentPage === 2 ? "active" : "")}
-        >
-          {props.pages[2]}
-        </a>
-      </li>
-      <li className="nav-item">
-        <a 
-        onClick={() => props.setPage(3)}
-        className={"nav-link " + (props.currentPage === 3 ? "active" : "")}
-        >
-          {props.pages[3]}
-        </a>
-      </li>
-      <li className="nav-item">
-        <a 
-        onClick={() => props.setPage(4)}
-        className={"nav-link " + (props.currentPage === 4 ? "active" : "")}
-        >
-          {props.pages[4]}
-        </a>
-      </li>
+      {
+        //the map method is great instead of a for loop in react
+        props.pages.map((item, index) => {
+          {/* key is a prop needed to add anytime you dynamically render */ }
+          return (
+            <li className="nav-item">
+              <a
+                onClick={() => props.setPage(index)}
+                className={"nav-link " + (props.currentPage === index ? "active" : "")}
+              >
+                {item}
+              </a>
+            </li>
+          )
+        })
+      }
     </ul>
+
   )
 }
 
